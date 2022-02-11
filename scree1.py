f05b08e9bcf76dcd84c53185092c8e0b30ef5300
Gilclei Araujo <gilclei.araujo@saude.ba.gov.br>
2022-02-11 15:42:26 -0300

    first commit

---
+++ scree1.py
@@ -0,0 +1,18 @@
+from lib2to3.pgen2 import driver
+from selenium import webdriver 
+from time import sleep 
+
+options = webdriver.ChromeOptions()
+options.add_argument('headless')
+options.add_argument('window-size=1920x1080')
+options.add_argument("disable-gpu")
+
+browser = webdriver.Chrome('chromedriver.exe', chrome_options=options) #chromedriver V98 verificar versão do chrome
+browser.get("http://intranet.saude.ba.gov.br/") 
+browser.execute_script("document.body.style.zoom='90%'")
+browser.set_window_size(1920,1080,browser.window_handles[0])
+browser.maximize_window()
+print(browser.title)
+sleep(5) 
+browser.get_screenshot_as_file(browser.title + "_screenshot.png") 
+browser.quit() 
